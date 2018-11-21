55137e384d0befc62afc26a3dbedfc6e4e2f0594
Steven Ott <stevenkott@icloud.com>
2018-11-21 02:32:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfnames)# code an argument here
+  dwarfnames.each_with_index do
 end
 
 def summon_captain_planet# code an argument here
