509f2a05a01fc95ac384e68113d6deee8ecbd649
Steven Ott <stevenkott@icloud.com>
2018-11-21 02:51:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,8 @@ end
 
 def find_the_cheese(strings)# code an argument here
    cheese_types = ["cheddar", "gouda", "camembert"] 
-  strings.include?(cheese_types)
+  strings.find do |x|
+    x == cheese_types
     
   end
 end
